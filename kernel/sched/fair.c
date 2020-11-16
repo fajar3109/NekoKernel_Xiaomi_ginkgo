f478c8f4864b487e10387d617fa664b893e33e5f
Tyler Nijmeh <tylernij@gmail.com>
2020-11-16 14:35:12 +0530

    sched/fair: Reduce latency
    [ghostrider-reborn] Taken from ktweak @ https://github.com/tytydraco/KTweak/commit/7f6bd5ea55ad34c5358ebda5ef330cf7e2623518
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -90,8 +90,13 @@ walt_dec_cfs_rq_stats(struct cfs_rq *cfs_rq, struct task_struct *p) {}
  *
  * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
  */
+<<<<<<< HEAD
 unsigned int sysctl_sched_latency			= 4000000ULL;
 unsigned int normalized_sysctl_sched_latency		= 4000000ULL;
+=======
+unsigned int sysctl_sched_latency			= 1000000ULL;
+unsigned int normalized_sysctl_sched_latency		= 1000000ULL;
+>>>>>>> 9c6d98106602 (sched/fair: Reduce latency)
 
 /*
  * Enable/disable honoring sync flag in energy-aware wakeups.
@@ -120,8 +125,8 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_N
  *
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity		= 400000ULL;
-unsigned int normalized_sysctl_sched_min_granularity	= 400000ULL;
+unsigned int sysctl_sched_min_granularity		= 100000ULL;
+unsigned int normalized_sysctl_sched_min_granularity	= 100000ULL;
 
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
@@ -143,8 +148,8 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
  *
  * (default: 1 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_wakeup_granularity		= 2000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity	= 2000000UL;
+unsigned int sysctl_sched_wakeup_granularity		= 500000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity	= 500000UL;
 
 const_debug unsigned int sysctl_sched_migration_cost	= 5000000UL;
 DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
