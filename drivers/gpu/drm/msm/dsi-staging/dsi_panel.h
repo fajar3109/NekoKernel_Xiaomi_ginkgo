7eb236dd988ec4e17a3816f79353dd02f75a2d39
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-15 19:34:39 +0530

    dsm/msm: dsi-staging: Add support for CABC mode
     * Content Adaptive Backlight Control
     * borrowed code from HBM
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: fajar <santuyz321@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -222,6 +222,7 @@ struct dsi_panel {
 
 #ifdef CONFIG_MACH_XIAOMI_GINKGO
 	int hbm_mode;
+	int cabc_mode;
 #endif
 };
 
@@ -347,6 +348,8 @@ void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
 
 #ifdef CONFIG_MACH_XIAOMI_GINKGO
 int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
+
+int dsi_panel_apply_cabc_mode(struct dsi_panel *panel);
 #endif
 
 #endif /* _DSI_PANEL_H_ */
