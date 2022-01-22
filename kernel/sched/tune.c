dea816cabe7b823de832f6535d09dd7ee20bd79a
fajar <santuyz321@gmail.com>
2022-01-22 17:36:02 +0000

    treewide: fix failed build
    Signed-off-by: fajar <santuyz321@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -682,7 +682,7 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 static int boost_write_wrapper(struct cgroup_subsys_state *css,
 			       struct cftype *cft, s64 boost)
 {
-	if (task_is_booster(current))
+	if (task_is_zygote(current))
 		return 0;
 
 	return boost_write(css, cft, boost);
@@ -691,7 +691,7 @@ static int boost_write_wrapper(struct cgroup_subsys_state *css,
 static int prefer_idle_write_wrapper(struct cgroup_subsys_state *css,
 				     struct cftype *cft, u64 prefer_idle)
 {
-	if (task_is_booster(current))
+	if (task_is_zygote(current))
 		return 0;
 
 	return prefer_idle_write(css, cft, prefer_idle);
